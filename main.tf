af8aac8cac0f7800cad2e509928e7ca6aaf4303e
Romain Deville <code@romaindeville.fr>
2023-12-22 11:08:14 +0100

    ✨ Add main.tf calling submodules

---
+++ main.tf
@@ -0,0 +1,23 @@
+module "user" {
+  source = "./user"
+
+  user_gpgkeys = var.user.gpgkeys
+  user_sshkeys = var.user.sshkeys
+}
+
+module "repos" {
+  source   = "./repos"
+  for_each = var.repos
+
+  repo                   = each.value.config
+  repo_labels            = each.value.labels
+  repo_actions_variables = each.value.variables
+  repo_tag_protection    = each.value.tag_protections
+}
+
+module "organization" {
+  source = "./organization"
+  count  = var.organization != null ? 1 : 0
+
+  organization = var.organization
+}
